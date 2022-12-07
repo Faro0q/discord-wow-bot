89df64f9327a06f9b33739de9375722fb34e4bc6
Farooq <syed@farooq.dev>
2022-12-07 15:09:44 -0600

    reformated more code

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-user_dict = {'farooqq':'0', 'zuruhgar':'0', 'btracks':'0', 'meatsmoothie':'0', 'setralanat':'0', 'rhsisbae':'0'}
+user_dict = {'farooqq':'0', 'zuruhgar':'0', 'btracks':'0', 'meatsmoothie':'0', 'setralanat':'0', 'rhcisbae':'0'}
 
 @client.event
 async def on_ready():
@@ -26,16 +26,13 @@ async def on_message(message):
     if message.content.startswith('!itemlevel'):
         access_token = create_access_token(os.environ["client_id"], os.environ["client_secret"])
         updated_dict = namespace_profile_us(access_token, "", 'equipped_item_level')
-        
-        
-        embed=discord.Embed(
-            title="List of Users Item Level: Thrall",
-            color=discord.Color.from_rgb(68,128,168),
-            description="This will display your current item level"
-        )
-        embed.timestamp = datetime.datetime.now()
-        embed.add_field(name="Name", value='\n'.join(updated_dict), inline="True")
-        embed.add_field(name="ilvl", value='\n'.join(updated_dict.values()), inline="True")
+
+        title="List of Users Item Level: Thrall",
+        color=discord.Color.from_rgb(68,128,168),
+        description="This will display your current item level"
+        embed_key="Name"
+        embed_value="ilvl"
+        embed = get_embed(title, color, description, embed_key, embed_value, updated_dict)
         await message.channel.send(embed=embed)
     
     # arena rating
@@ -43,15 +40,12 @@ async def on_message(message):
         access_token = create_access_token(os.getenv('client_id'), os.getenv('client_secret'))
         updated_dict = namespace_profile_us(access_token, "/pvp-bracket/2v2", 'rating')
         
-        # await message.channel.send(ss)
-        embed=discord.Embed(
-            title="List of Users 2v2 Arena Rating: Thrall",
-            color=discord.Color.from_rgb(176, 9, 9),
-            description="This will display your arena rating"
-        )
-        embed.timestamp = datetime.datetime.now()
-        embed.add_field(name="Name", value='\n'.join(updated_dict), inline="True")
-        embed.add_field(name="Rating", value='\n'.join(updated_dict.values()), inline="True")
+        title="List of Users 2v2 Arena Rating: Thrall",
+        color=discord.Color.from_rgb(176, 9, 9),
+        description="This will display your arena rating"
+        embed_key="Name"
+        embed_value="Rating"
+        embed = get_embed(title, color, description, embed_key, embed_value, updated_dict)
         await message.channel.send(embed=embed)
 
 
@@ -68,11 +62,20 @@ def namespace_profile_us(wow_token, arena_url, json_var):
 
     # sort the dictionary by value in descending order
     sorted_dict = sorted(user_dict, key=user_dict.get, reverse=True)
-
     # create a new dictionary using the sorted keys
     new_dict = dict((key, user_dict[key]) for key in sorted_dict)
-
     return new_dict
+
+def get_embed(title, color, description, embed_key, embed_value, updated_dict):
+    embed=discord.Embed(
+            title=title,
+            color=color,
+            description=description
+        )
+    embed.timestamp = datetime.datetime.now()
+    embed.add_field(name=embed_key, value='\n'.join(updated_dict), inline="True")
+    embed.add_field(name=embed_value, value='\n'.join(updated_dict.values()), inline="True")
+    return embed
 
 def create_access_token(client_id, client_secret, region = 'us'):
     data = { 'grant_type': 'client_credentials' }
